75e5dc5d0426decf34a9aa76c3239c456e19d497
Lennu Vuolanne <vuolanne.lennu@gmail.com>
2022-07-18 18:29:46 +0300

    new

--- src/main.tsx
+++
@@ -1,9 +0,0 @@
-import { h, render } from "preact";
-import App from "./components/App";
-
-const root = document.getElementById("root")
-if (root === null) {
-    console.error("Failed to find root element (#root)")
-} else {
-    render(<App />, root) 
-}
