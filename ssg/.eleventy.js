d1ca68055c3a4324cdd499f74b9e16f1aaaf3c8a
Lennu Vuolanne <vuolanne.lennu@gmail.com>
2023-10-27 18:38:16 +0300

    modify i18n to a global data file instead

--- ssg/.eleventy.js
+++ ssg/.eleventy.js
@@ -25,25 +25,7 @@ const fetchI18n = async () => {
 
   const res = await axios.post(process.env.CMS_URL, i18nQuery);
 
-  const locales = res.data.data.text.reduce(
-    ({ fi, en, sv }, { key, text_fi, text_en, text_sv }) => ({
-      fi: {
-        ...fi,
-        [key]: text_fi,
-      },
-      en: {
-        ...en,
-        [key]: text_en,
-      },
-      sv: {
-        ...sv,
-        [key]: text_sv,
-      },
-    }),
-    {}
-  );
-
-  await fs.mkdir(LOCALES_DIR, { recursive: true });
+  const locales = await fs.mkdir(LOCALES_DIR, { recursive: true });
 
   await fs.writeFile(
     path.join(LOCALES_DIR, "fi.json"),
@@ -66,53 +48,11 @@ module.exports = (config) => {
     console.log("BUILDING TO PREFIX " + process.env.PATH_PREFIX);
   }
 
-  config.on("eleventy.before", () =>
-    fetchI18n().catch((e) => console.error("FAILED TO FETCH LOCALES: " + e))
-  );
-
-  config.on("eleventy.after", () => {
-    return esbuild.build({
-      entryPoints: ["src/lib/calendar.js"],
-      alias: {
-        react: "preact/compat",
-        "react-dom": "preact/compat",
-      },
-      outdir: "_site/lib",
-      minify: false,
-      bundle: true,
-      sourcemap: true,
-    });
-  });
-
   config.addWatchTarget("./src/site");
 
   config.addPassthroughCopy({ "./src/img": "img" });
 
-  config.addPlugin(EleventyRenderPlugin);
-
   config.addPlugin(EleventyHtmlBasePlugin);
-
-  const i18n = new I18n({
-    locales: ["fi", "en", "sv"],
-    defaultLocale: "fi",
-    directory: LOCALES_DIR,
-    updateFiles: false,
-    logErrorFn: console.error,
-    logWarnFn: console.warn,
-    logDebugFn: console.debug,
-  });
-
-  config.addTransform("translate", async function (content) {
-    i18n.setLocale(this.page.locale);
-    return content.replace(/t:([\w:]+)/g, (_, key) => {
-      const translation = i18n.__(key);
-      if (translation === undefined) {
-        console.warn("Could not find translation with key " + key);
-        return key;
-      }
-      return translation;
-    });
-  });
 
   return {
     pathPrefix: process.env.PATH_PREFIX || "",
