221242fdce4382c65f4af540b98779c41b58195b
Lennu Vuolanne <vuolanne.lennu@gmail.com>
2024-03-27 13:08:07 +0200

    progress

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,24 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+import withBundleAnalyzer from "@next/bundle-analyzer";
+import withLitSSR from "@lit-labs/nextjs";
+
+let nextConfig = {
+  transpilePackages: ["@mui/material-next"],
+};
+
+// Toggle these variables to enable/disable plugins
+const useBundleAnalyzer = false;
+const useLitSSR = false;
+
+if (useBundleAnalyzer) {
+  nextConfig = withBundleAnalyzer({
+    enabled: process.env.ANALYZE === "true",
+  })(nextConfig);
+}
+
+if (useLitSSR) {
+  nextConfig = withLitSSR()(nextConfig);
+}
 
 export default nextConfig;
